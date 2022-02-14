39a7ee8ea907e8f5b82b72a65caa086581e556ff
Teb <teb88@github.com>
2022-02-13 23:14:43 -0300

    add header metadata

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import MainView from '@components/MainView';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="h-full bg-light">
-      <MainView />
-    </div>
+    <>
+      <Head>
+        <title>WordTeb (Wordle en Español) - Adivina la palabra</title>
+        <meta
+          name="description"
+          content="Clon del Wordle adaptado al español. Deberás adivinar cada día una nueva palabra descubriendo las letras que corresponden a base de prueba y error"
+        />
+      </Head>
+      <div className="h-full bg-light dark:bg-dark">
+        <MainView />
+      </div>
+    </>
   );
 }
